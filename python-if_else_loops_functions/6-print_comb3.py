a350d0b74305cf11313d383c666c9836335348ba
NekodaMushi <4252@holbertonschool.com>
2022-09-06 11:56:16 +0000

    Indentation issue solved

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for i in range(0,9):
+for i in range(0,10):
 	j = i + 1
-	while (j < 9):
-		print("{}{}".format(i,j), end = ', ')
+	while (j < 10):
+		if i == 8:
+			print("89")
+		else:
+			print("{}{}".format(i,j), end = ', ')
 		j += 1
-print(89)
