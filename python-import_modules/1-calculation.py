d32d654e02107871bf50d3de7f91b8163d267998
NekodaMushi <4252@holbertonschool.com>
2022-09-08 12:10:06 +0000

    Add with format

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    
+    print("{} + {} = {}".format(a, b, (a + b)))
+    print("{} - {} = {}".format(a, b, (a - b)))
+    print("{} * {} = {}".format(a, b, (a * b)))
+    print("{} / {} = {}".format(a, b, (a / b)))
