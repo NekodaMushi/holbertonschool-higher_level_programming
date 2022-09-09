baf433cba7382aa88912e88765d13609f483bea2
NekodaMushi <4252@holbertonschool.com>
2022-09-09 11:58:14 +0000

    Indentation into space

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ import sys
 
 if __name__ == "__main__":
 
-	sum = 0
-	for x in range(1, len(sys.argv)):
-		sum += int(sys.argv[x])
-	print(sum)
+    sum = 0
+    for x in range(1, len(sys.argv)):
+        sum += int(sys.argv[x])
+    print(sum)
