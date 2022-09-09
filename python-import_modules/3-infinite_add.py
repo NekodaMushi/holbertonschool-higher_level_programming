5ed86cd9d0f86110911094bddf7d06567dfb1734
NekodaMushi <4252@holbertonschool.com>
2022-09-09 11:55:41 +0000

    sum with argv

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-#if __name__ == __"main"__
+if __name__ == "__main__":
 
-sum = 0
-for x in range(1, len(sys.argv)):
-    sum += int(sys.argv[x])
-print(sum)
+	sum = 0
+	for x in range(1, len(sys.argv)):
+		sum += int(sys.argv[x])
+	print(sum)
