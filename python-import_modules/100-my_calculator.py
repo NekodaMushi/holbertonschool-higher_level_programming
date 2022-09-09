d2f1225af17ea3ed7186dad963174f2058f2e18e
NekodaMushi <4252@holbertonschool.com>
2022-09-09 13:28:33 +0000

    Dont understand why checker said that

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
         
     n = len(sys.argv)
 
@@ -12,7 +12,8 @@ if __name__ == "__main__":
 
     elif sys.argv[2] not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
-
+        sys.exit(1)
+        
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
