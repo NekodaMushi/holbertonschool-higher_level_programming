0b3a50f0255a8e8f0f29fb7a76db4108c89c700d
NekodaMushi <4252@holbertonschool.com>
2022-09-12 12:17:50 +0000

    Please work

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for val in row:
+            if val != 0:
+                print('', end="")
             print("{:d}".format(val), end=" ")
         print('')
-    print('')
