4552c0715767b5ddff711e7011b9bdeab3728a99
NekodaMushi <4252@holbertonschool.com>
2022-09-13 13:18:59 +0000

    no need for len

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,7 @@
 def uniq_add(my_list=[]):
     sum = 0
     new_list = []
-    for i in range(len(my_list)):
+    for i in my_list:
         if i not in new_list:
             new_list.append(i)
     
