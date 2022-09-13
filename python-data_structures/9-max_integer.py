f1b3fe3030ff2261d7ab9957cfd68bb2a4bd8ba1
NekodaMushi <4252@holbertonschool.com>
2022-09-13 07:52:54 +0000

    declaration put higher

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,8 +5,11 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
+    maxi = my_list[0]
     for i in my_list:
-        maxi = my_list[0]
         if my_list[i] > maxi:
             maxi = my_list[i]
         return maxi
+
+#You could get the same result by sorting 
+# the list then retrieve the last element of list
