8e0201579576b4b6e1452cc5024581bf704883c5
NekodaMushi <4252@holbertonschool.com>
2022-09-13 13:14:56 +0000

    only uniq integers are added

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""adds all unique integers in a list (only once for each integer)"""
+
+
+def uniq_add(my_list=[]):
+    sum = 0
+    new_list = []
+    for i in range(len(my_list)):
+        if i not in new_list:
+            new_list.append(i)
+    
+    for a in new_list:
+        sum += a
+    return sum
