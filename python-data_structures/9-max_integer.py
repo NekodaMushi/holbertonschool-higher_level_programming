b7b57a11fac947a29a808972a085eca06a139f66
NekodaMushi <4252@holbertonschool.com>
2022-09-13 08:01:24 +0000

    Sorting is shorter

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,11 +5,4 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    maxi = my_list[0]
-    for i in my_list:
-        if my_list[i] > maxi:
-            maxi = my_list[i]
-        return maxi
-
-#You could get the same result by sorting 
-# the list then retrieve the last element of list
+    return sorted(my_list)[-1]
