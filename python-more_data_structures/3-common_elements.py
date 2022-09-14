b502df415640c98201fd2bdc547e4b18e942fdd6
NekodaMushi <4252@holbertonschool.com>
2022-09-14 08:29:39 +0000

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """returns a set of common elements in two sets"""
 
+
 def common_elements(set_1, set_2):
-    for a in set_1:
-        for b in set_2:
-            if a == b:
-                 return b
+    return(set(set_1).intersection(set_2))
