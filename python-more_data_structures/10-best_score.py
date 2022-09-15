cef3f901d8fe36287b4aa5ce5aaea6c53a30a3a8
NekodaMushi <4252@holbertonschool.com>
2022-09-15 13:54:12 +0000

    testing something

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,5 +5,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    maximum_val = max(a_dictionary, key=a_dictionary.get)
-    return maximum_val
+    return max(a_dictionary, key=a_dictionary.get)
+
