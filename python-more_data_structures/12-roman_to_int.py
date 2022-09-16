b00086a71f2fd4af5d7bd0b84a2340c949af222c
NekodaMushi <4252@holbertonschool.com>
2022-09-16 11:46:30 +0000

    Roman

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 
 
 def roman_to_int(roman_string):
-    if not roman_string == None:
+    if roman_string == None:
         return 0
     
     dic = {
@@ -17,8 +17,8 @@ def roman_to_int(roman_string):
     }
     decimal = 0
     for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and dic[roman_string[i]] > dic[roman_string[i+1]]:
-            decimal -= dic[roman_string[i]] 
+        if i + 1 < len(roman_string) and dic[roman_string[i]] < dic[roman_string[i+1]]:
+            decimal -= dic[roman_string[i]]
         else:
             decimal += dic[roman_string[i]]
     return decimal
