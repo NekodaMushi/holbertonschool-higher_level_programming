bc1771ec493e1ca48a2598a41b27a1946aad5bd2
NekodaMushi <4252@holbertonschool.com>
2022-09-16 11:37:37 +0000

    Roman

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Converts a Roman numeral to an integer."""
+
+
+def roman_to_int(roman_string):
+    if not roman_string == None:
+        return 0
+    
+    dic = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    }
+    decimal = 0
+    for i in range(len(roman_string)):
+        if i + 1 < len(roman_string) and dic[roman_string[i]] > dic[roman_string[i+1]]:
+            decimal -= dic[roman_string[i]] 
+        else:
+            decimal += dic[roman_string[i]]
+    return decimal
