09868ef37be3157e25cf0e88c242f4cf874e3649
NekodaMushi <4252@holbertonschool.com>
2022-09-17 13:54:38 +0000

    PLEASING PEP8

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -18,9 +18,9 @@ def roman_to_int(roman_string):
     decimal = 0
     
     for i in range(len(roman_string)):
-        if i + 1 < len(roman_string):
-            if dic[roman_string[i]] < dic[roman_string[i+1]]:
-                decimal -= dic[roman_string[i]]
+        if i + 1 < len(roman_string) or
+                dic[roman_string[i]] < dic[roman_string[i+1]]:
+            decimal -= dic[roman_string[i]]
         else:
             decimal += dic[roman_string[i]]
     return decimal
