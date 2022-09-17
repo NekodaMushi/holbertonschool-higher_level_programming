5a3733c524845d66762fdb8858bb839db4e96865
NekodaMushi <4252@holbertonschool.com>
2022-09-17 13:48:39 +0000

    Pleasing PEP8

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,21 +3,24 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string == None or isinstance(roman_string, int):
+    if roman_string is None or isinstance(roman_string, int):
         return 0
-    
+
     dic = {
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     decimal = 0
+    condition1 = i + 1 < len(roman_string)
+    condition2 = dic[roman_string[i]] < dic[roman_string[i+1]]
+    
     for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and dic[roman_string[i]] < dic[roman_string[i+1]]:
+        if condition1 and condition2:
             decimal -= dic[roman_string[i]]
         else:
             decimal += dic[roman_string[i]]
