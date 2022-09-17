d2cd62936bc39695baa66c352d1743b8aa270d16
NekodaMushi <4252@holbertonschool.com>
2022-09-17 14:55:45 +0000

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,9 +4,9 @@
 
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
+        res = a / b
+    except:
+        res = None
     finally:
-        print("Inside result: {}".format(result))
-    return result
+        print("Inside result: {}".format(res))
+    return res
