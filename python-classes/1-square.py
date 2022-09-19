98d254b3fe548f3241a52486e85f99e840757fc4
NekodaMushi <4252@holbertonschool.com>
2022-09-19 11:38:37 +0000

    Square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""First time defining a class"""
 
 
 class Square:
     """class Square that defines a square"""
     _Square__size = 3
-    def __init__(self, size):
-        self.__size = size
 
+    def __init__(self, size):
+        """Size of square initialized"""
+        self.__size = size
