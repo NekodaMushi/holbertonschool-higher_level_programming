b453a883334f5d938fdaa2fadd363490525a45a1
NekodaMushi <4252@holbertonschool.com>
2022-09-20 13:30:35 +0000

    Prettier

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,23 +3,46 @@
 
 
 class Square:
-    """class Square that defines a square"""
+    """Square Class"""
 
     def __init__(self, size=0):
-        """Initiate square"""
+        """
+        Init a square
+        Args:
+            size: the size of square
+        Return: None
+        """
         self.__size= size
 
     @property 
     def size(self):
-        """retrieve size"""
+        """
+        A getter
+        Return: self.__size
+        """
         return self.__size
+    
     @size.setter
     def size(self, value):
+        """
+        Set the size of the square
+        Args:
+            value (int): The new value of the size
+        Raises:
+            TypeError: Error if not an int
+            ValueError: Error if not positiv
+        Returns: None
+        """
         if type(value) and not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+            
     def area(self):
-        return int(self.__size**2)
+        """
+        Calcul area of the square
+        Returns: The area of the square
+        """
+        return int(self.__size ** 2)
