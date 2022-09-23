d586d83c31af9b37ee804ea4cec2e45b9c9db674
NekodaMushi <4252@holbertonschool.com>
2022-09-23 14:08:10 +0000

    cleaning code

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,10 +15,9 @@ def print_square(size):
 
     Returns: Nothing """
 
-    if type(size) is not int:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float):
-        raise TypeError("size must be an integer")
+
     print(("#" * size + "\n") * size, end="")
