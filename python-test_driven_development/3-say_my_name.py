8920135101967b73847e0ecffb51773b0a424a44
NekodaMushi <4252@holbertonschool.com>
2022-09-23 11:58:12 +0000

    Failed test added

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,16 +5,15 @@
 def say_my_name(first_name, last_name=""):
     """ Function checking if variables
     are strings
-    
+
     Raises exception if not a string
-    
+
     Print string
-    
+
     Returns: Nothing """
-    
+
     if type(first_name) is not str:
         raise("first_name must be a string")
     if type(last_name) is not str:
         raise("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
-
