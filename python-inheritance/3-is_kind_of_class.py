5d6f9683cb92351581a64b28e5859f0c34b02889
NekodaMushi <4252@holbertonschool.com>
2022-09-27 09:31:37 +0000

    Well isintance has some advantages

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Check if object is an instance
+of a specific class or inherited from
+a specific class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Method checking if object is instance"""
+    return isinstance(obj, a_class)
