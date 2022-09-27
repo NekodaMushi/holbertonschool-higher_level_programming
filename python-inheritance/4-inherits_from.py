5da0581cf66f2af3d6bd8d3a72e5935019f1e69d
NekodaMushi <4252@holbertonschool.com>
2022-09-27 09:38:25 +0000

    is it sub class?

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Check if object is an instance 
+of a class that inherited"""
+
+
+def inherits_from(obj, a_class):
+    """Method checking if object is instance"""
+    return issubclass(obj, a_class)
