f06abba1c6574f554f0e06e58a232a4fc62213cb
NekodaMushi <4252@holbertonschool.com>
2022-09-27 12:56:07 +0000

    Ending

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -4,14 +4,14 @@
 
 class MyInt(int):
     """Rebel Class"""
-    
+
     def __eq__(self, rebel):
         """__eq__ method to compare two 
         objects by their values"""
 
         return int(self) != rebel
-    
+
     def __ne__(self, rebel):
         """Negating return value"""
-        
+
         return int(self) == rebel
