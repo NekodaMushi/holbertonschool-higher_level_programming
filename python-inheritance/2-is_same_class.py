bb190c8d717f46efc9ae5b70fa018e5fd648aaec
NekodaMushi <4252@holbertonschool.com>
2022-09-27 09:28:08 +0000

    type better than isinstance it seems

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Check if object is exactly an instance
+of a specific class"""
+
+
+def is_same_class(obj, a_class):
+    """Method checking if object is instance"""
+    return type(obj) == a_class
+
