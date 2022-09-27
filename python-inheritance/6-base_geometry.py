672f709b2d2bf55d790ea0d9513529dee3267f6f
NekodaMushi <4252@holbertonschool.com>
2022-09-27 06:01:03 +0000

    Only exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry Function"""
+
+
+class BaseGeometry:
+    """Mother class for Geometric Object"""
+
+    def area(self):
+        """Raise exception"""
+    raise Exception("area() is not implemented")
