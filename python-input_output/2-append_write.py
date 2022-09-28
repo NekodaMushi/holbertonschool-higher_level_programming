f4cc896ccc05c45b7d204aac37d69787bb8cefa1
NekodaMushi <4252@holbertonschool.com>
2022-09-28 10:00:16 +0000

    appending

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function reading file
+Returns: Nb of characters"""
+
+
+
+def append_write(filename="", text=""):
+    """Open file and display nb of char"""
+    with open(filename, mode='a', encoding="UTF8") as testa:
+        testa.write(text)
+        return len(text)
