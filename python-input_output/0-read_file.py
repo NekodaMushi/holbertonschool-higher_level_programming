a1d3f45086a710b2c45227f0fb6dc719594099b9
NekodaMushi <4252@holbertonschool.com>
2022-09-28 08:56:30 +0000

    For loop

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     """Open file and display content into STDout"""
-    with open(filename, 'r', encoding="utf-8" ) as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         file = file.read()
         for fi in file:
             print(fi, end="")
