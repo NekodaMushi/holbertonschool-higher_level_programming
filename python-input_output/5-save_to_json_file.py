2ca008897557d8aa30434b07a97a99836d0e43ae
NekodaMushi <4252@holbertonschool.com>
2022-09-29 06:35:41 +0000

    Serializable

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Learning how to deal with JSON"""
+from encodings import utf_8
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file,
+    using a JSON representation"""
+
+    with open(filename, "w", encoding="utf8") as f:
+        json.dump(my_obj, f)
