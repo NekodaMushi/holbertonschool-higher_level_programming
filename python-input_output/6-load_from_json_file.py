1f72160e30e668bfe43876cfa1129d43c59c0020
NekodaMushi <4252@holbertonschool.com>
2022-09-29 09:52:46 +0000

    JSON data file convert to Python Data Object

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Learning how to use JSON"""
+import json
+
+
+def load_from_json_file(filename):
+    """Object from a “JSON file”"""
+
+    with open(filename, "r", encoding="UTF8") as f:
+        json.load(f)
