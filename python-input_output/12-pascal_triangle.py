7c0a27cc3d6f74536de8a0c2d7a3df9b5a821cae
NekodaMushi <4252@holbertonschool.com>
2022-09-30 13:18:19 +0000

    Index Way to solve it

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -18,8 +18,8 @@ def pascal_triangle(n):
 
     for i in range(n):
         tmp = []
-        for j in range (i+1):
-            if j == 0 or j == i :
+        for j in range(i+1):
+            if j == 0 or j == i:
                 tmp.append(1)
             else:
                 tmp.append(triangle[i-1][j-1]+triangle[i-1][j])
