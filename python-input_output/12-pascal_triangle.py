e2b918a78caa1067a54b22c37979488600bbaec8
NekodaMushi <4252@holbertonschool.com>
2022-09-30 13:14:23 +0000

    Index Way to solve it

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""module that representing the Pascal’s triangle of n"""
+
+
+def pascal_triangle(n):
+    """
+    function that represents the Pascal’s triangle of n
+
+    Args:
+        n: number of iteration of the triangle
+
+    Returns: (list)
+        empty list if n <= 0
+        Pascals triangle of n
+    """
+    n = 5
+    triangle = []
+
+    for i in range(n):
+        tmp = []
+        for j in range (i+1):
+            if j == 0 or j == i :
+                tmp.append(1)
+            else:
+                tmp.append(triangle[i-1][j-1]+triangle[i-1][j])
+        triangle.append(tmp)
+    return triangle
