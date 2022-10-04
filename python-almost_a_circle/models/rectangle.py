8f1d831da0c86e8283175ff8d9c11c88e4d4b57a
NekodaMushi <4252@holbertonschool.com>
2022-10-04 10:45:37 +0000

    getting longer to do

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,3 +22,74 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+
+    @property
+    def width(self):
+        """Getter of width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Getter of height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Getter of x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """Getter of y"""
+        return self.__y
+
+
+    @height.setter
+    def height(self, value):
+        """Defining height of Rectangle
+        Checking if input is not integer
+        if sides < 0
+        Returning error if so"""
+        self.valid_attribs("height", value)
+        self.__height = value
+        
+    @width.setter
+    def width(self, value):
+        """Defining width of Rectangle
+        Checking if input is not integer
+        if sides < 0
+        Returning error if so"""
+
+        self.valid_attribs("width", value)
+        self.__width = value
+
+    @x.setter
+    def x(self, value):
+        """Defining x of Rectangle
+        Checking if value < 0
+        Returning error if so"""
+        self.valid_attribs("x", value)
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Defining y of Rectangle
+        Checking if value < 0
+        Returning error if so"""
+        self.valid_attribs("y", value)
+        self.__y = value
+
+    def valid_attribs(self, name, value):
+        """Validates value
+        args:
+            name: always a string
+            value: an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if name == "width" or name == "height":
+            if value <= 0:
+                raise ValueError("{} must be > 0".format(name))
+        if name == "x" or name == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(name))
