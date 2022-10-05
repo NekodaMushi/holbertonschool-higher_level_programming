21fb7c8230434e71c02563def83c98cea48b1fa8
NekodaMushi <4252@holbertonschool.com>
2022-10-05 08:51:36 +0000

    try assigns argument to each attribute

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Class inheriting from Base"""
-
-
 from models.base import Base
 
 
@@ -113,3 +111,16 @@ class Rectangle(Base):
         """Return string representation of object"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
 {self.width}/{self.height}"
+
+    def update(self, *args):
+        """Assigns an argument to each attribute"""
+        if args:
+            try:
+                self.id = args[0]
+                self.width = args[1]
+                self.height = args[2]
+                self.x = args[3]
+                self.y = args[4]
+            except IndexError:
+                pass
+
