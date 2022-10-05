9b3dc9314d94228bedc3c1c3c36ac65dcab499bc
NekodaMushi <4252@holbertonschool.com>
2022-10-05 12:17:25 +0000

    Squarring

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,13 +17,28 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """Getter of Square Size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter of Square Size"""
         self.width = value
         self.height = value
 
     def __str__(self):
         """String description of object"""
         return(f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}")
+
+    def update(self, *args, **kwargs):
+        """Assigns attributes"""
+        try:
+            self.id = args[0]
+            self.size = args[1]
+            self.x = args[2]
+            self.y = args[3]
+        except IndexError:
+            pass
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
