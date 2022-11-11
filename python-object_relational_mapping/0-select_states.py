9678a641b3ad048d0efcdbd0f7929d2f10aa5571
NekodaMushi <4252@holbertonschool.com>
2022-11-11 10:49:06 +0000

    chmod

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python3
-
-"""
-Script to list all states
-from database hbtn_0e_0usa
-where name of states matches an argument of command line
-"""
-
-import MySQLdb
+""" script that lists all states from the database hbtn_0e_0_usa """
 import sys
+import MySQLdb
 
-if "__main__" == __name__:
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        passwd="root",
-        db=sys.argv[3],
-        charset="utf8"
-    )
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE '{:s}' \
-            ORDER BY states.id ASC".format(
-            sys.argv[4])
-    )
-    rows = cur.fetchall()
-    for row in rows:
-        if row[1] == sys.argv[4]:
-            print(row)
-    cur.close()
-    db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
