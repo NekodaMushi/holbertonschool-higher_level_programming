4813fcf7e1d1e0d8dfae34260bd92afcd4baab0c
NekodaMushi <4252@holbertonschool.com>
2022-12-06 11:45:02 +0000

    semistandard

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -5,4 +5,4 @@ const num = Number(process.argv[2]);
 Number.isInteger(num) ? console.log(`My number: ${num}`) : console.log('Not a number');
 // const num = process.argv[2]
 // num.isNaN ? console.log('Not a number') : console.log(`My number: ${num}`)
-//not working
+// not working
