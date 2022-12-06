2a8636a85a541142e694345f7eadd2e29649d814
NekodaMushi <4252@holbertonschool.com>
2022-12-06 15:05:33 +0000

    Semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-'user strict'
+'user strict';
 
 // base case
-function factorial(n){
-if (n == 0 || n == 1) { return 1;}
-// recursion case
-else { return n * factorial(n - 1)}
+function factorial (n) {
+  if (n === 0 || n === 1) { return 1; }
+  // recursion case
+  else { return n * factorial(n - 1); }
 }
-console.log(factorial(Number(process.argv[2])))
+console.log(factorial(Number(process.argv[2])));
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 'user strict';
 
-function add(a, b)
-{
-  return a+b;
+function add (a, b) {
+  return a + b;
 }
 console.log(add(Number(process.argv[2]), Number(process.argv[3])));
