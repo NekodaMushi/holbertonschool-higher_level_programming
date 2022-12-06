e9707d4c5339ad198e55f211b7d877ffbb88d4ac
NekodaMushi <4252@holbertonschool.com>
2022-12-06 11:43:00 +0000

    js validation

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-'user strict'
+'use strict';
 
-const num = Number(process.argv[2])
-Number.isInteger(num) ? console.log(`My number: ${num}`) : console.log('Not a number')
+const num = Number(process.argv[2]);
+Number.isInteger(num) ? console.log(`My number: ${num}`) : console.log('Not a number');
 // const num = process.argv[2]
 // num.isNaN ? console.log('Not a number') : console.log(`My number: ${num}`)
 //not working
