b42da3d4a1a136076e0d525be9ef37217a9c9fe8
NekodaMushi <4252@holbertonschool.com>
2022-12-07 12:58:45 +0000

    jonas is king

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-'use strict'
+'use strict';
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  for (let i=0; i < list.length; i++){
-    if (list[i] === searchElement){
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
       count += 1;
     }
   }
   return count;
-}
+};
