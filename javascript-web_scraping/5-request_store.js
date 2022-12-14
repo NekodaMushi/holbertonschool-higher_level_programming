bccf620136ac62f178e534f180e5ff49dea8f135
NekodaMushi <4252@holbertonschool.com>
2022-12-14 09:46:07 +0000

    indentation can be a headache sometimes

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a GET request to the URL
+request(url, function (error, response, content) {
+  if(!error && response.statusCode === 200) {
+    fs.writeFile(filePath, content, 'utf-8', err => {
+      if (err){
+        console.log(err);
+      }
+    });
+  }  else {
+    console.log(error);
+  }
+  });
+
